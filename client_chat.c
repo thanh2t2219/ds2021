6e5d15e24f17e55e740a500e8057eff8a7944689
thanh2t2219 <thanh2t405@yahoo.com>
2021-03-01 01:09:31 +0700

    Send string instead of character for chat system

--- client_chat.c
+++ client_chat.c
@@ -1,42 +1,62 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <unistd.h>
 #include <string.h>
 #include <sys/types.h>
 #include <sys/socket.h>
+#include <netinet/in.h>
 #include <netdb.h>
 
-int main(int argc, char* argv[]) {
-    int so;
-    char buffer[100];
-    struct sockaddr_in ad;
+int main(int argc, char *argv[])
+{
+    // declaration
+    int client_socket, n, portno;
+    struct sockaddr_in server_addr;
+    struct hostent *server;
 
-    socklen_t ad_length = sizeof(ad);
-    struct hostent *hep;
+    char buffer[1024];
+
+    if (argc < 3) {
+       fprintf(stderr,"usage %s hostname port\n", argv[0]);
+       exit(0);
+    }
+    portno = atoi(argv[2]);
 
     // create socket
-    int serv = socket(AF_INET, SOCK_STREAM, 0);
+    client_socket = socket(AF_INET, SOCK_STREAM, 0);
+
 
     // init address
-    hep = gethostbyname(argv[1]);
-    memset(&ad, 0, sizeof(ad));
-    ad.sin_family = AF_INET;
-    ad.sin_addr = *(struct in_addr *)hep->h_addr_list[0];
-    ad.sin_port = htons(2222);
+    server = gethostbyname(argv[1]);
+
+    bzero((char *) &server_addr, sizeof(server_addr));
+
+    server_addr.sin_family = AF_INET;
+    bcopy((char *)server->h_addr,
+         (char *)&server_addr.sin_addr.s_addr,
+         server->h_length);
+    server_addr.sin_port = htons(portno);
+
 
     // connect to server
-    connect(serv, (struct sockaddr *)&ad, ad_length);
+    connect(client_socket,(struct sockaddr *) &server_addr,sizeof(server_addr));
+    printf("Client: ");
 
-    while (1) {
+    while(1)
+    {
         // after connected, it's client turn to chat
 
         // send some data to server
-        printf("Client>");
-        scanf("%s", buffer);
-        write(serv, buffer, strlen(buffer) + 1);
+        bzero(buffer,1024);
+        fgets(buffer,1023,stdin);
+        n = write(client_socket,buffer,strlen(buffer));
 
         // then it's server turn
-        read(serv, buffer, sizeof(buffer));
+        bzero(buffer,1024);
+        n = read(client_socket,buffer,1023);
 
-        printf("Server says: %s\n", buffer);
+        printf("Server : %s\n",buffer);
     }
+    close(client_socket);
+    return 0;
 }
--- server_chat.c
+++ server_chat.c
@@ -1,55 +1,75 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <unistd.h>
 #include <sys/types.h>
 #include <sys/socket.h>
-#include <netdb.h>
+#include <netinet/in.h>
 
-int main() {
-    int socket_server, cli, pid;
-    struct sockaddr_in addr;
-    char buffer[1024];
-    socklen_t ad_length = sizeof(addr);
+int main()
+{
+    // declaration
+    int server_socket, cli, pid;
+    char buffer[1023];
+    struct sockaddr_in server_addr, client_addr;
+    int n;
+    socklen_t cli_addr_length = sizeof(client_addr);
 
-    // create the socket
-    socket_server = socket(AF_INET, SOCK_STREAM, 0);
+    // create a socket
+    server_socket = socket(AF_INET, SOCK_STREAM, 0);
+
 
     // bind the socket to port 2222
-    memset(&addr, 0, sizeof(addr));
-    addr.sin_family = AF_INET;
-    addr.sin_addr.s_addr = INADDR_ANY;
-    addr.sin_port = htons(2222);
-    bind(socket_server, (struct sockaddr *)&addr, ad_length);
+    bzero((char *) &server_addr, sizeof(server_addr));
+
+    server_addr.sin_family = AF_INET;
+    server_addr.sin_addr.s_addr = INADDR_ANY;
+    server_addr.sin_port = htons(2222);
+
+    bind(server_socket, (struct sockaddr *) &server_addr,
+              sizeof(server_addr));
 
     // then listen
-    listen(socket_server, 0);
+    listen(server_socket,0);
 
-    while (1) {
+    while(1)
+    {
         // an incoming connection
-        cli = accept(socket_server, (struct sockaddr *)&addr, &ad_length);
+        cli = accept(server_socket,
+                 (struct sockaddr *) &client_addr,
+                 &cli_addr_length);
 
         pid = fork();
-        if (pid == 0) {
+
+        if(pid == 0) {
             // I'm the son, I'll serve this client
             printf("client connected\n");
-            while (1) {
+
+            while(1) {
                 // it's client turn to chat, I wait and read message from client
-                read(cli, buffer, sizeof(buffer));
+                bzero(buffer,1024);
+                n = read(cli,buffer,1023);
                 printf("Client says: %s\n",buffer);
 
                 // now it's my (server) turn
-                printf("Server>", buffer);
-                scanf("%s", buffer);
-                write(cli, buffer, strlen(buffer) + 1);
+                bzero(buffer,1024);
+                fgets(buffer,1023,stdin);
+                n = write(cli,buffer,strlen(buffer));
+                printf("Server says: %s\n", buffer);
+
             }
+
             return 0;
         }
+
         else {
             // I'm the father, continue the loop to accept more clients
             continue;
         }
-    }
+
+     }
+
     // disconnect
     close(cli);
-
+    close(server_socket);
 }
